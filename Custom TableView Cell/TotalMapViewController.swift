230fb9aba074403f7b9a4feb8bb18d611bd1773f
D7703_17 <d7703_17@d7703-17ui-imac.local>
2018-06-11 15:50:18 +0900

    add map view

---
+++ Custom TableView Cell/TotalMapViewController.swift
@@ -0,0 +1,37 @@
+//
+//  TotalMapViewController.swift
+//  Custom TableView Cell
+//
+//  Created by D7703_17 on 2018. 6. 11..
+//  Copyright © 2018년 박금상. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class TotalMapViewController: UIViewController {
+
+      @IBOutlet weak var totalMapView: MKMapView!
+      override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
